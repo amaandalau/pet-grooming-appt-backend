44c9c7b32ab81ca665f724f7805265a60a1d0e78
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-07-02 13:18:42 +0800

    Fix DB structure & datatypes

--- db/migrations.prod.sql
+++ db/migrations.prod.sql
@@ -18,8 +18,9 @@ USE pet_grooming_appt_booking_system_prod;
 CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(255),
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
+    is_verified BOOLEAN,
     role ENUM('owner', 'groomer') NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -31,43 +32,19 @@ CREATE TABLE pets(
     name VARCHAR(255) NOT NULL,
     date_of_birth DATE NOT NULL,
     species VARCHAR(255) NOT NULL,
-    breed VARCHAR(255) NOT NULL,
+    breed VARCHAR(255),
     weight_in_kg DOUBLE NOT NULL,
     color VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY(id)
-);
-
-CREATE TABLE appointments(
-    id INT NOT NULL AUTO_INCREMENT,
-    date DATE NOT NULL,
-    special_instructions VARCHAR(255),
-    status ENUM('pending', 'confirmed', 'in-progress', 'completed', 'cancelled') NOT NULL,
     owner_id INT NOT NULL,
-    pet_id INT NOT NULL,
-    groomer_id INT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (id),
-    FOREIGN KEY (owner_id) REFERENCES users(id),
-    FOREIGN KEY (pet_id) REFERENCES pets(id),
-    FOREIGN KEY (groomer_id) REFERENCES users(id)
-);
-
-CREATE TABLE appointment_services(
-    id INT NOT NULL AUTO_INCREMENT,
-    services ENUM('Bath & Shampoo', 'Cut & Style', 'Nail Trimming', 'Brush & De-Shed', 'Ear Cleaning') NOT NULL,
-    appointment_id INT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (id),
-    FOREIGN KEY (appointment_id) REFERENCES appointments(id)
+    PRIMARY KEY(id),
+    FOREIGN KEY (owner_id) REFERENCES users(id)
 );
 
 CREATE TABLE timeslots(
     id INT NOT NULL AUTO_INCREMENT,
-    timestamp TIMESTAMP NOT NULL,
+    slots TIMESTAMP NOT NULL,
     status ENUM('Available', 'Not Available', 'Out-of-office') NOT NULL,
     groomer_id INT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -76,10 +53,38 @@ CREATE TABLE timeslots(
     FOREIGN KEY (groomer_id) REFERENCES users(id)
 );
 
+CREATE TABLE appointments(
+    id INT NOT NULL AUTO_INCREMENT,
+    appt_date TIMESTAMP NOT NULL,
+    special_instructions VARCHAR(255),
+    status ENUM('pending', 'confirmed', 'in-progress', 'completed', 'cancelled') NOT NULL DEFAULT 'pending',
+    owner_id INT NOT NULL,
+    pet_id INT NOT NULL,
+    groomer_id INT NOT NULL,
+    timeslot_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    FOREIGN KEY (owner_id) REFERENCES users(id),
+    FOREIGN KEY (pet_id) REFERENCES pets(id),
+    FOREIGN KEY (groomer_id) REFERENCES users(id),
+    FOREIGN KEY (timeslot_id) REFERENCES timeslots(id)
+);
+
+CREATE TABLE appt_services(
+    id INT NOT NULL AUTO_INCREMENT,
+    services ENUM('Bath & Shampoo', 'Cut & Style', 'Nail Trimming', 'Brush & De-Shed', 'Ear Cleaning') NOT NULL,
+    appt_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    FOREIGN KEY (appt_id) REFERENCES appointments(id)
+);
+
 CREATE TABLE reviews(
     id INT NOT NULL AUTO_INCREMENT,
-    ratings INT NOT NULL,
-    CONSTRAINT check_ratings CHECK(ratings>=1 AND ratings <=5),
+    rating INT NOT NULL,
+    CONSTRAINT check_ratings CHECK(rating >= 1 AND rating <= 5),
     title VARCHAR(255) NOT NULL,
     description VARCHAR(255),
     owner_id INT NOT NULL,
