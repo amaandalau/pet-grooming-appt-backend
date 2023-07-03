8d17f1636813077a35816103bb424184c6c36f79
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-07-03 15:54:09 +0800

    Add services routes

---
+++ routes/services.routes.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+const serviceController = require("../controllers/services.controllers.js")
+
+router.get('/', serviceController.getAllServices)
+
+router.get('/:serviceID', serviceController.getServicesByID)
+
+router.post('/', serviceController.createService)
+
+router.put('/:serviceID', serviceController.updateService)
+
+router.delete('/:serviceID', serviceController.deleteService)
