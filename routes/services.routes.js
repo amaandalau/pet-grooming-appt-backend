67b8b18c65c648e6da78dfc2b20d067cf4aa2503
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-07-04 12:36:08 +0800

    Add auth for services routes

--- routes/services.routes.js
+++ routes/services.routes.js
@@ -1,15 +1,31 @@
 const express = require('express')
 const router = express.Router()
 const serviceController = require("../controllers/services.controllers.js")
+const { verifyToken, checkRole } = require('../middlewares/auth.middleware.js')
 
 router.get('/', serviceController.getAllServices)
 
 router.get('/:serviceID', serviceController.getServicesByID)
 
-router.post('/', serviceController.createService)
+router.post(
+    '/', 
+    verifyToken,
+    checkRole(['admin', 'groomer']),
+    serviceController.createService
+)
 
-router.put('/:serviceID', serviceController.updateService)
+router.put(
+    '/:serviceID', 
+    verifyToken,
+    checkRole(['admin', 'groomer']),
+    serviceController.updateService
+)
 
-router.delete('/:serviceID', serviceController.deleteService)
+router.delete(
+    '/:serviceID', 
+    verifyToken,
+    checkRole(['admin', 'groomer']),
+    serviceController.deleteService
+)
 
 module.exports = router
