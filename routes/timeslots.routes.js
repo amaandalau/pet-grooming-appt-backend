a2567de5f278b5dfaa81223dfeb3e600c072620e
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-07-04 12:36:30 +0800

    Add auth for timeslots routes

--- routes/timeslots.routes.js
+++ routes/timeslots.routes.js
@@ -1,16 +1,32 @@
 const express = require('express')
 const router = express.Router()
 const timeslotController = require("../controllers/timeslots.controllers.js")
+const { verifyToken, checkRole } = require('../middlewares/auth.middleware.js')
 
 router.get('/timeslots', timeslotController.getAllTimeslots)
 
 // Get specific groomer's (by id) specific timeslot id
 router.get('/users/:userID/timeslots/:timeslotID', timeslotController.getTimeslotByID)
 
-router.post('/timeslots', timeslotController.createTimeslot)
+router.post(
+    '/timeslots', 
+    verifyToken,
+    checkRole(['admin', 'groomer']),
+    timeslotController.createTimeslot
+)
 
-router.put('/:timeslotID', timeslotController.updateTimeslot)
+router.put(
+    '/:timeslotID', 
+    verifyToken,
+    checkRole(['admin', 'groomer']),
+    timeslotController.updateTimeslot
+)
 
-router.delete('/:timeslotID', timeslotController.deleteTimeslot)
+router.delete(
+    '/:timeslotID',
+    verifyToken,
+    checkRole(['admin', 'groomer']),
+    timeslotController.deleteTimeslot
+)
 
 module.exports = router
