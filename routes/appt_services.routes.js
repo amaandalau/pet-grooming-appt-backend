66cd4c969dfb9bb44329ced30e220e06869400d9
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-07-04 12:37:16 +0800

    Add auth for appt services routes

--- routes/appt_services.routes.js
+++ routes/appt_services.routes.js
@@ -1,16 +1,31 @@
 const express = require('express')
 const router = express.Router()
 const apptServicesController = require("../controllers/appt_services.controllers.js")
+const { verifyToken, checkRole } = require('../middlewares/auth.middleware.js')
 
 router.get('/', apptServicesController.getAllApptServices)
 
 router.get('/:apptServiceID', apptServicesController.getApptServiceByID)
 
-router.create('/', apptServicesController.createApptService)
+router.create(
+    '/', 
+    verifyToken,
+    checkRole(['admin', 'owner']),
+    apptServicesController.createApptService
+)
 
-router.put('/:apptServiceID', apptServicesController.updateApptService)
+router.put(
+    '/:apptServiceID', 
+    verifyToken,
+    checkRole(['admin', 'owner']),
+    apptServicesController.updateApptService
+)
 
-router.delete('/:apptServiceID', apptServicesController.deleteApptService)
-
+router.delete(
+    '/:apptServiceID', 
+    verifyToken,
+    checkRole(['admin', 'owner']),
+    apptServicesController.deleteApptService
+)
 
 module.exports = router
